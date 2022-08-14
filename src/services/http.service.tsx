e7eaebf089d35e4dc1aeabe1247559051ce3bdd7
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-08-14 18:40:00 -0300

    [*] added interceptor token

--- src/services/http.service.tsx
+++ src/services/http.service.tsx
@@ -1,12 +1,36 @@
 import axios from 'axios'
 import appsettings from 'common/config/appsettings.json'
+import storageService from './storage.service'
 
 const HttpService = axios.create({
   baseURL: appsettings.API,
   headers: {
     'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Type': 'application/json',
   },
+})
+
+function BuildAuth() {
+  const accessToken = storageService.getAccessToken()
+  return accessToken
+}
+
+HttpService.interceptors.request.use((config: any) => {
+  const authentication = BuildAuth() || ''
+  if (authentication !== null) {
+    config.headers['Authorization'] = 'Bearer ' + authentication
+  }
+
+  return config
+})
+
+HttpService.interceptors.response.use((config) => {
+  const authentication = BuildAuth() || ''
+  if (authentication !== null) {
+    config.headers['Authorization'] = 'Bearer ' + authentication
+  }
+
+  return config
 })
 
 export { HttpService }
