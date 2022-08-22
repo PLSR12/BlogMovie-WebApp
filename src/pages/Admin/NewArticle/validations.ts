29498ae2dd65e30d4556fdf6d7fc57a2f13ead0c
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-08-21 21:05:51 -0300

    [+] created validation

---
+++ src/pages/Admin/NewArticle/validations.ts
@@ -0,0 +1,11 @@
+import * as Yup from 'yup'
+
+export const NewArticleSchema = Yup.object().shape({
+  title: Yup.string().required('O campo "Titúlo" é obrigatório'),
+  preview: Yup.string().required('O campo "Preview" é obrigatória'),
+  content: Yup.string().required('O campo "Conteudo" é obrigatório'),
+  category: Yup.object().required('Escolha uma categoria'),
+  file: Yup.mixed().test('required', 'Carregue uma imagem', (value: any) => {
+    return value && value.length > 0
+  }),
+})
