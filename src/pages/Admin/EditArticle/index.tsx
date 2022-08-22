8b1c2613342871a635865b0a456b7d7e99bd2bc7
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-08-21 21:05:20 -0300

    [+] added page edit

---
+++ src/pages/Admin/EditArticle/index.tsx
@@ -0,0 +1,182 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import CloudUploadIcon from '@mui/icons-material/CloudUpload'
+import * as Atoms from 'components/Atoms'
+import * as Organisms from 'components/Organisms'
+import { ArticlesInput } from 'models/IArticles'
+import { CategoriesAll } from 'models/ICategories'
+import { useEffect, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { useHistory, useParams } from 'react-router-dom'
+import ReactSelect from 'react-select'
+
+import GenericModal from 'components/Organisms/Modal/GenericModal'
+import { ModalContentLoading } from 'components/Organisms/Modal/style'
+
+import { ArticlesService } from 'services/Articles.service'
+import CategoryService from 'services/Categories.service'
+import { ToastService } from 'services/toast.service'
+import * as S from './styles'
+import { EditArticleSchema } from './validations'
+
+interface ArticleDefault {
+  title: string
+  preview: string
+  content: string
+}
+
+export default function EditArticle() {
+  const [fileName, setFileName] = useState(null)
+  const [article, setArticle] = useState<any>()
+  const [categories, setCategories] = useState<CategoriesAll[]>()
+  const [modalLoadingIsOpen, setModalLoadingIsOpen] = useState<boolean>(true)
+  const history = useHistory()
+  const { id } = useParams<{ id: any }>()
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<ArticlesInput>({ resolver: yupResolver(EditArticleSchema) })
+
+  const onSubmit = async (data: ArticlesInput) => {
+    try {
+      const articleFormData = new FormData()
+
+      articleFormData.append('title', data.title)
+      articleFormData.append('preview', data.preview)
+      articleFormData.append('content', data.content)
+      articleFormData.append('category_id', data.category.id)
+      articleFormData.append('file', data.file[0])
+
+      await ArticlesService.insert(articleFormData)
+
+      ToastService.success('Artigo criado com sucesso')
+      setTimeout(() => {
+        history.push('/admin-articles')
+      }, 3000)
+    } catch (error) {
+      ToastService.error('Erro ao criar artigo ,tente novamente mais tarde')
+    }
+  }
+
+  useEffect(() => {
+    async function oneArticle() {
+      try {
+        const splitedId = id.split(':')[1]
+
+        const OneArticle = await ArticlesService.getById(splitedId).then(
+          (response: any) => response
+        )
+
+        setArticle(OneArticle)
+      } catch (error) {
+        ToastService.error('Erro ao buscar artigo, tente novamente mais tarde')
+      }
+    }
+    oneArticle()
+  }, [id])
+
+  useEffect(() => {
+    async function loadCategory() {
+      try {
+        const allCategories = await CategoryService.getAll().then(
+          (response: any) => response
+        )
+
+        setCategories(allCategories)
+        setModalLoadingIsOpen(false)
+      } catch (error) {
+        ToastService.error(
+          'Erro ao buscar as Categorias,tente novamente mais tarde'
+        )
+      }
+    }
+    loadCategory()
+  }, [])
+
+  useEffect(() => {
+    setValue('title', article?.title)
+    setValue('preview', article?.preview)
+    setValue('content', article?.content)
+  }, [article])
+
+  return (
+    <>
+      <GenericModal isOpen={modalLoadingIsOpen}>
+        <ModalContentLoading>
+          <h2>Carregando...</h2>
+        </ModalContentLoading>
+      </GenericModal>
+      <S.Container>
+        <Organisms.Box>
+          <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+            <Atoms.InputComponent
+              text="Titúlo:"
+              register={register}
+              htmlFor="title"
+              error={errors.title}
+            />
+            <Atoms.TextAreaComponent
+              text="Preview:"
+              register={register}
+              htmlFor="preview"
+              error={errors.preview}
+            />
+            <Atoms.TextAreaComponent
+              text="Contéudo:"
+              register={register}
+              htmlFor="content"
+              error={errors.content}
+            />
+            <div>
+              <S.LabelUpload>
+                {fileName || (
+                  <>
+                    <CloudUploadIcon />
+                    Caregue a imagem da notícia
+                  </>
+                )}
+                <input
+                  type="file"
+                  {...register('file')}
+                  onChange={(value: any): void => {
+                    setFileName(value.target.files[0]?.name)
+                  }}
+                />
+              </S.LabelUpload>
+              <Organisms.ErrorMessage>
+                {errors.file?.message}
+              </Organisms.ErrorMessage>
+            </div>
+            <div>
+              <Controller
+                name="category"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <ReactSelect
+                      {...field}
+                      options={categories}
+                      getOptionLabel={(ctg: { id: any; name: string }) =>
+                        ctg.name
+                      }
+                      getOptionValue={(ctg: { id: any }) => ctg.id}
+                      placeholder="Escolha uma categoria"
+                      defaultValue={article?.category}
+                    />
+                  )
+                }}
+              ></Controller>
+              <Organisms.ErrorMessage>
+                {errors.category?.message}
+              </Organisms.ErrorMessage>
+            </div>
+            <S.ButtonStyle type="submit"> Adicionar Noticia </S.ButtonStyle>
+          </form>
+        </Organisms.Box>
+      </S.Container>
+    </>
+  )
+}
---
+++ src/pages/Admin/EditArticle/styles.ts
@@ -0,0 +1,34 @@
+import { Button } from 'components/Atoms/Button'
+import styled from 'styled-components'
+
+export const Container = styled.div``
+export const Label = styled.p`
+  font-size: 15px;
+  color: #fff;
+  margin-bottom: 5px;
+`
+
+export const ButtonStyle = styled(Button)`
+  width: 100%;
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-top: 25px;
+  background-color: #2779f5;
+`
+export const LabelUpload = styled.label`
+  margin-top: 35px;
+  margin-bottom: 15px;
+  cursor: pointer;
+  display: flex;
+  border: 1px #000 dashed;
+  border-radius: 5px;
+  padding: 35px;
+  gap: 5px;
+  align-items: center;
+  color: #000;
+
+  input {
+    width: 1px;
+    opacity: 0;
+  }
+`
