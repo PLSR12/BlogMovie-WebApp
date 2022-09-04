e0297e598f87e2ca1e6c67bd2427399255569b39
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-09-04 10:27:48 -0300

    [*] deleted sideBarNav admin

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -2,8 +2,6 @@ import * as S from './styles'
 
 import paths from '../../common/constants/paths'
 
-import * as Organisms from '../../components/Organisms'
-
 import EditArticle from './EditArticle'
 import ListArticles from './ListArticles'
 import NewArticles from './NewArticle'
@@ -12,7 +10,6 @@ import NewCategories from './NewCategory'
 export function Admin({ match: { path } }: any) {
   return (
     <S.Container>
-      <Organisms.SideMenuAdmin />
       <S.ContainerItems>
         {path === paths.AdminArticles && <ListArticles />}
         {path === paths.NewArticles && <NewArticles />}
