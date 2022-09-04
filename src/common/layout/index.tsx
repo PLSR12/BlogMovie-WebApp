28c61e354e8ec5d16b2d7b8fe96e4334ea26428f
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-09-04 10:25:29 -0300

    [*] updated layout

--- src/common/layout/index.tsx
+++ src/common/layout/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { ReactNode } from 'react'
+
+import * as Molecules from 'components/Molecules'
+import * as Organisms from 'components/Organisms'
 
 import { Body, Container, ContainerPage } from './styles'
 
-export default function Layout({ children }: { children: any }) {
+export default function Layout({ children }: { children?: ReactNode }) {
   return (
     <Container>
+      <Organisms.SideMenuAdmin />
       <ContainerPage>
+        <Molecules.Header />
         <Body>{children}</Body>
       </ContainerPage>
     </Container>
