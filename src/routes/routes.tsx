f8fb946db9ff7e4628707fba0ea560f4d16b87fe
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-09-04 10:28:06 -0300

    [*] added layout in routes for admin

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom'
 
 import { Admin, Article, Home, Login } from '../pages'
 
+import Layout from 'common/layout'
 import paths from '../common/constants/paths'
 
 import PrivateRoutes from './private-routes'
@@ -13,10 +14,12 @@ export default function RoutesPath() {
       <Route exact path={paths.OneArticle} component={Article} />
       <Route exact path={paths.Login} component={Login} />
 
-      <PrivateRoutes exact path={paths.AdminArticles} component={Admin} />
-      <PrivateRoutes exact path={paths.NewArticles} component={Admin} />
-      <PrivateRoutes exact path={paths.NewCategories} component={Admin} />
-      <PrivateRoutes exact path={paths.EditArticle} component={Admin} />
+      <Layout>
+        <PrivateRoutes exact path={paths.AdminArticles} component={Admin} />
+        <PrivateRoutes exact path={paths.NewArticles} component={Admin} />
+        <PrivateRoutes exact path={paths.NewCategories} component={Admin} />
+        <PrivateRoutes exact path={paths.EditArticle} component={Admin} />
+      </Layout>
     </Switch>
   )
 }
