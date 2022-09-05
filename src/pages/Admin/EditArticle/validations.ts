eb030b2b3a80073a2b70de102dce4cb3c279dd5a
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-09-05 17:23:23 -0300

    [*] added validation category

--- src/pages/Admin/EditArticle/validations.ts
+++ src/pages/Admin/EditArticle/validations.ts
@@ -4,7 +4,7 @@ export const EditArticleSchema = Yup.object().shape({
   title: Yup.string().required('O campo "Titúlo" é obrigatório'),
   preview: Yup.string().required('O campo "Preview" é obrigatória'),
   content: Yup.string().required('O campo "Conteudo" é obrigatório'),
-  category: Yup.object().required('Escolha uma categoria'),
+  category: Yup.string().required('Escolha uma categoria'),
   file: Yup.mixed().test('required', 'Carregue uma imagem', (value: any) => {
     return value && value.length > 0
   }),
