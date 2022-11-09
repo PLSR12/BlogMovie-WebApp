b1168eea8245df3a6d6f8d424f6ae5605ce37731
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-11-09 20:19:02 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import Routes from './routes/routes'
+import { ToastContainer } from 'react-toastify'
 import GlobalStyle from './common/styles/global'
 import AppProvider from './context'
-import { ToastContainer } from 'react-toastify'
+import Routes from './routes/routes'
 
 function App() {
   return (
